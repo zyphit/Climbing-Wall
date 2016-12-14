122c7d868e4b04bc3b82b32218d8efbdfc529d9f
Zyphit <zyphit@users.noreply.github.com>
2016-12-14 15:13:12 -0800

    Final version before building the hardware
    Tested this software with full breadboard setup of the final project.  Time to solder!
    Arduino Nano

--- climbing-wall.ino
+++ climbing-wall.ino
@@ -1,8 +1,11 @@
 //Fun program to run the winning buttons on the climbing wall.  The wall has three arcade buttons along the top
-//and pushing each will light up eight LEDs on the wall, buttons and LED sets will be colour coded (red, yellow,
+//and pushing each will light up eight LEDs on the wall. Buttons and LED sets will be colour coded (red, yellow,
 //green), plus an external rocker switch to control power.  Each set of LEDs will have a shift register (74HC595)
-//controlling them, so three digital outputs per colour, plus three digital inputs for switches, 12 pins total.
-//based on code courtesy of EEEnthusiast.com
+//controlling them, so three outputs per colour, plus three digital inputs for switches, 12 pins total.
+//based on code courtesy of EEEnthusiast.com.
+//
+//Uses Arduino nano, three 74HC595 shift registers, 24 LEDs (8 ea of green, yellow, red), and three momentary
+//push buttons.
 
 int grn_DS_pin = 12;
 int grn_STCP_pin = 11;
@@ -12,30 +15,48 @@ int red_DS_pin = 9;
 int red_STCP_pin = 8;
 int red_SHCP_pin = 7;
 
-int yl_DS_pin = 6;
-int yl_STCP_pin = 5;
-int yl_SHCP_pin = 4;
+int yl_DS_pin = A0;
+int yl_STCP_pin = A1;
+int yl_SHCP_pin = A2;
 
-int grn_button = 3;
-int red_button = 2;
-int yl_button = 1;
+int grn_button = 4;
+int red_button = 3;
+int yl_button = 2;
+
+int sensor_value = HIGH; //checks state of button presses (low = pressed)
 
 boolean registers[8];   //stores LED output states for shift register
 
 void setup()
 {
-  // Set pins 4-12 as outputs to control 24 LEDs
-  for (int i = 4;i<13;i++)
-    pinMode(i,OUTPUT);
-  // Set pins 1-3 as inputs with internal pullup resistors for switches
-  for (int i = 1;i<=4;i++)
+  // Set pins 7-12 and A0-A2 as outputs to control 24 LEDs
+  pinMode(A0,OUTPUT);
+  pinMode(A1,OUTPUT);
+  pinMode(A2,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(12,OUTPUT);
+
+  // Set pins 2-4 as inputs with internal pullup resistors for switches
+  for (int i =2;i<=4;i++)
     pinMode(i,INPUT_PULLUP);
-  writereg(1,2,3);
+
+  // turn off all ights to start    
+  for(int i = 0; i<8; i++)
+    {
+      registers[i]= LOW;
+    }
+    writereg(grn_DS_pin,grn_STCP_pin,grn_SHCP_pin);
+    writereg(red_DS_pin,red_STCP_pin,red_SHCP_pin);
+    writereg(yl_DS_pin,yl_STCP_pin,yl_SHCP_pin);
 }
 
 void loop() //check for each button press and then light up the corresponding LEDs
 {
-int sensor_value = digitalRead(grn_button);
+sensor_value = digitalRead(grn_button);
 if (sensor_value == HIGH) {
     delay(5);
   } else {
@@ -57,7 +78,7 @@ if (sensor_value == HIGH) {
     }
 }
 
-//uploads to shift register the current settings in 'registers[]'
+//function that uploads to shift register the current settings in 'registers[]'
 void writereg(int DS_pin, int STCP_pin, int SHCP_pin)
 {
 digitalWrite(STCP_pin, LOW);
@@ -70,7 +91,7 @@ for (int i = 7; i>=0; i--)
 digitalWrite(STCP_pin, HIGH);
 }
 
-//function that blinks LEDs on a shift register.  Three colours to choose from
+//function that blinks LEDs on a shift register.  Lights up either green or red or yellow depending on input parameters
 void LEDblink(int DS_pin, int STCP_pin, int SHCP_pin)
 {
     for(int j = 0; j<3; j++)                    //repeat pattern j times
