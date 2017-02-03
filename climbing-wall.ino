4e83315191abe0e3eb6ae5ae28067528dbaca558
Zyphit <zyphit@users.noreply.github.com>
2017-02-02 22:14:49 -0800

    Add files via upload
    (Hopefully) last changes for style and readability.

--- climbing-wall.ino
+++ climbing-wall.ino
@@ -36,15 +36,15 @@ void setup()
   pinMode(A0,OUTPUT);
   pinMode(A1,OUTPUT);
   pinMode(A2,OUTPUT);
-  for (int i=7; i<=12; i++)
+  for(int i=7; i<=12; i++)
     pinMode(i,OUTPUT);
   
   // Set pins 2-4 as inputs with internal pullup resistors for switches
-  for (int i =2;i<=4;i++)
+  for(int i=2; i<=4; i++)
     pinMode(i,INPUT_PULLUP);
 
   // turn off all ights to start    
-  for(int i = 0; i<8; i++)
+  for(int i=0; i<8; i++)
       registers[i]= LOW;
   writereg(grn_DS_pin,grn_STCP_pin,grn_SHCP_pin);
   writereg(red_DS_pin,red_STCP_pin,red_SHCP_pin);
@@ -55,7 +55,7 @@ void loop() //check for each button press and then light up the corresponding LE
 {
 sensor_value = digitalRead(grn_button);
 if (sensor_value == HIGH) {
-    delay(5);
+    delay(3);
   } else {
     LEDblink(grn_DS_pin,grn_STCP_pin,grn_SHCP_pin);
     LEDrandom(grn_DS_pin,grn_STCP_pin,grn_SHCP_pin);
@@ -64,7 +64,7 @@ if (sensor_value == HIGH) {
 
 sensor_value = digitalRead(red_button);
 if (sensor_value == HIGH) {
-    delay(5);
+    delay(3);
   } else {
     LEDblink(red_DS_pin,red_STCP_pin,red_SHCP_pin);
     LEDrandom(red_DS_pin,red_STCP_pin,red_SHCP_pin);
@@ -73,7 +73,7 @@ if (sensor_value == HIGH) {
 
 sensor_value = digitalRead(yl_button);
 if (sensor_value == HIGH) {
-    delay(5);
+    delay(3);
   } else {
     LEDblink(yl_DS_pin,yl_STCP_pin,yl_SHCP_pin);
     LEDrandom(yl_DS_pin,yl_STCP_pin,yl_SHCP_pin);
@@ -85,7 +85,7 @@ if (sensor_value == HIGH) {
 void writereg(int DS_pin, int STCP_pin, int SHCP_pin)
 {
 digitalWrite(STCP_pin, LOW);
-for (int i = 7; i>=0; i--)
+for(int i=7; i>=0; i--)
   {
     digitalWrite(SHCP_pin, LOW);
     digitalWrite(DS_pin, registers[i] );
@@ -97,23 +97,23 @@ digitalWrite(STCP_pin, HIGH);
 //function that blinks LEDs on a shift register.  Lights up either green or red or yellow depending on input parameters
 void LEDblink(int DS_pin, int STCP_pin, int SHCP_pin)
 {
-  for(int j = 0; j<4; j++)                    //repeat pattern j times
+  for(int j=0;j<4; j++)                    //repeat pattern j times
     {
-    for(int i = 0; i<8; i++)                    //cycle LEDs from 0-7
+    for(int i=0; i<8; i++)                    //cycle LEDs from 0-7
     {
       registers[i] = HIGH;
       registers[i-1] = LOW;
       delay(100);
       writereg(DS_pin,STCP_pin,SHCP_pin);
     }
-    for(int i = 7; i>=0; i--)                   //cycle LEDs from 7-0
+    for(int i=7; i>=0; i--)                   //cycle LEDs from 7-0
     {
       registers[i] = HIGH;
       registers[i+1] = LOW;
       writereg(DS_pin,STCP_pin,SHCP_pin);
       delay(100);
     }
-    for(int i = 0; i<8; i++)                    // turn off all ights at the end of the loop    
+    for(int i=0; i<8; i++)                    // turn off all ights at the end of the loop    
       registers[i]= LOW;
     writereg(DS_pin,STCP_pin,SHCP_pin);
   }
@@ -122,7 +122,7 @@ void LEDblink(int DS_pin, int STCP_pin, int SHCP_pin)
 //function that blinks LEDs randomly with an increasing rate.
 void LEDrandom(int DS_pin, int STCP_pin, int SHCP_pin)
 {
-    for(int i = 25; i=1; i--)
+    for(int i=25; i=1; i--)
     {
       int randomLED = random(0,7);
       registers[randomLED] = HIGH;
@@ -136,13 +136,13 @@ void LEDrandom(int DS_pin, int STCP_pin, int SHCP_pin)
 //function that blinks all eight LEDs on and off 3 times.
 void threeblink(int DS_pin, int STCP_pin, int SHCP_pin)
 {
-    for (int j = 0; j<4; j++)
+    for(int j=0; j<4; j++)
     {
-      for(int i = 0; i<8; i++)
+      for(int i=0; i<8; i++)
         registers[i] = HIGH;
       writereg(DS_pin,STCP_pin,SHCP_pin);
       delay(100);
-      for(int i = 0; i < 8; i++)
+      for(int i=0; i<8; i++)
         registers[i] = LOW;
       writereg(DS_pin,STCP_pin,SHCP_pin);
       delay(50);
